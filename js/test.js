db8131fb4cb0ab6ad9f1a1f25b255b4db054b8d6
Adam Jonze <mail@jonze.io>
2018-04-20 17:51:51 +0100

    dels

--- js/test.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-$(".inputs").keyup(function() {
-  if (this.value.length == this.maxLength) {
-    $(this).nextAll('.inputs:enabled:first').focus();
-  }
-});
-
-});
